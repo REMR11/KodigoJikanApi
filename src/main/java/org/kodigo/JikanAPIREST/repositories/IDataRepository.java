d4b762acaba812ccd695b971b795677aba29865b
remr11 <ronaldmejia0211@gmail.com>
2025-01-01 16:44:44 -0600

    Feat: Crea metodo para buscar anime por url en IDataRepository.java

--- src/main/java/org/kodigo/JikanAPIREST/repositories/IDataRepository.java
+++ src/main/java/org/kodigo/JikanAPIREST/repositories/IDataRepository.java
@@ -4,6 +4,9 @@ import org.kodigo.JikanAPIREST.entities.Data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IDataRepository extends JpaRepository<Data, Long> {
+    Optional<Data> findByUrl(String url);
 }
